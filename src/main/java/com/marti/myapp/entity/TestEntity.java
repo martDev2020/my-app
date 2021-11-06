14488adbd79df6fe2364bce3a39ed5a8a466f71a
unknown <al221310188@gmail.com>
2021-11-05 19:23:41 -0600

    Nombre del proyecto: my-app. Descripción: prueba de mi primera versionamiento de aplicación en Java

---
+++ src/main/java/com/marti/myapp/entity/TestEntity.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package com.marti.myapp.entity;
+
+/**
+ * @author Martín
+ *
+ */
+public class TestEntity {
+	/**
+	 * Propiedad de prueba para la muestra de la compilación del archivo testEntity.
+	 * */
+	private String nonmbre;
+/**
+	 * @return the nonmbre
+	 */
+	public String getNonmbre() {
+		return nonmbre;
+	}
+
+	/**
+	 * @param nonmbre the nonmbre to set
+	 */
+	public void setNonmbre(String nonmbre) {
+		this.nonmbre = nonmbre;
+	}
+
+
+
+}
